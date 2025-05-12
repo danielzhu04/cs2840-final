d649183f2d71b8e87c9a22167c167e0ea6fb2ba2
lwinstony8 <l.winstony8@gmail.com>
2025-05-12 15:04:38 -0400

    daniel

--- RFdiffusion/design_partialdiffusion_core.sh
+++ RFdiffusion/design_partialdiffusion_core.sh
@@ -8,8 +8,8 @@
 #     diffuser.partial_T=5 \
 #     'contigmap.provide_seq=[0-227,515-1149]' \
 ./scripts/run_inference.py \
-    inference.output_prefix=data/RFdiffusion_output/esm3_abl1b_complete_partialdiffusion_core_t5_again \
+    inference.output_prefix=data/RFdiffusion_output/esm3_abl1b_complete_partialdiffusion_core_t50_again \
     inference.input_pdb=data/ESM_original/esm3_abl1b_complete.pdb \
     'contigmap.contigs=["A1-228/287/A516-1149"]' \
     inference.num_designs=1 \
-    diffuser.partial_T=5 \
+    diffuser.partial_T=50 \
