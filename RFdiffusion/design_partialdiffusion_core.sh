5bfe17dc69f553ecf1a67b74c422c11b4ef9dd59
lwinstony8 <l.winstony8@gmail.com>
2025-05-13 00:17:25 -0400

    batch script

--- RFdiffusion/design_partialdiffusion_core.sh
+++ RFdiffusion/design_partialdiffusion_core.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-
-# ./scripts/run_inference.py \
-#     inference.output_prefix=data/RFdiffusion_output/esm3_abl1b_complete_partialdiffusion_core_t5 \
-#     inference.input_pdb=data/ESM_original/esm3_abl1b_complete.pdb \
-#     'contigmap.contigs=["1149-1149"]' \
-#     inference.num_designs=1 \
-#     diffuser.partial_T=5 \
-#     'contigmap.provide_seq=[0-227,515-1149]' \
 ./scripts/run_inference.py \
-    inference.output_prefix=data/RFdiffusion_output/esm3_abl1b_complete_partialdiffusion_core_t50_again \
+    inference.output_prefix=data/RFdiffusion_output/t1/esm3_abl1b_complete_partialdiffusion_core \
     inference.input_pdb=data/ESM_original/esm3_abl1b_complete.pdb \
     'contigmap.contigs=["A1-228/287/A516-1149"]' \
-    inference.num_designs=1 \
-    diffuser.partial_T=50 
+    inference.num_designs=100 \
+    diffuser.partial_T=1
